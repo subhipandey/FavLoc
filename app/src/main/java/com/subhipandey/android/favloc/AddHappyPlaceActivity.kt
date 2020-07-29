ff5e0aedcf0847623a1c9a0d60a1f3c223b2a726
Subhi Pandey <subhi2012cpi@gmail.com>
2020-07-29 15:29:23 +0530

    Added Functionality to select image from gallery

--- app/src/main/java/com/subhipandey/android/favloc/AddHappyPlaceActivity.kt
+++ app/src/main/java/com/subhipandey/android/favloc/AddHappyPlaceActivity.kt
@@ -1,12 +1,15 @@
 package com.subhipandey.android.favloc
 
+import android.app.Activity
 import android.app.AlertDialog
 import android.app.DatePickerDialog
 import android.content.ActivityNotFoundException
+import android.content.ContentUris
 import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.provider.Settings
 import android.view.View
 import android.widget.Toast
@@ -16,30 +19,48 @@ import com.karumi.dexter.PermissionToken
 import com.karumi.dexter.listener.PermissionRequest
 import com.karumi.dexter.listener.multi.MultiplePermissionsListener
 import kotlinx.android.synthetic.main.activity_add_happy_place.*
+import java.io.IOException
 import java.text.SimpleDateFormat
 import java.util.*
 import java.util.jar.Manifest
 
 class AddHappyPlaceActivity : AppCompatActivity(), View.OnClickListener {
 
+    
     private var cal = Calendar.getInstance()
+
+
     private lateinit var dateSetListener: DatePickerDialog.OnDateSetListener
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
+
+
         setContentView(R.layout.activity_add_happy_place)
+
         setSupportActionBar(toolbar_add_place)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
         toolbar_add_place.setNavigationOnClickListener {
             onBackPressed()
         }
-        dateSetListener = DatePickerDialog.OnDateSetListener { View, year, month, dayOfMonth ->
-            cal.set(Calendar.YEAR, year)
-            cal.set(Calendar.MONTH, month)
-            cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
-            updateDateInView()
-        }
+
+
+        dateSetListener =
+            DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
+                cal.set(Calendar.YEAR, year)
+                cal.set(Calendar.MONTH, monthOfYear)
+                cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+
+                updateDateInView()
+            }
+
         et_date.setOnClickListener(this)
+
         tv_add_image.setOnClickListener(this)
+
     }
 
     override fun onClick(v: View?) {
@@ -48,11 +69,14 @@ class AddHappyPlaceActivity : AppCompatActivity(), View.OnClickListener {
                 DatePickerDialog(
                     this@AddHappyPlaceActivity,
                     dateSetListener,
+
                     cal.get(Calendar.YEAR),
                     cal.get(Calendar.MONTH),
                     cal.get(Calendar.DAY_OF_MONTH)
                 ).show()
             }
+
+
             R.id.tv_add_image -> {
                 val pictureDialog = androidx.appcompat.app.AlertDialog.Builder(this)
                 pictureDialog.setTitle("Select Action")
@@ -64,11 +88,7 @@ class AddHappyPlaceActivity : AppCompatActivity(), View.OnClickListener {
                     when (which) {
 
                         0 -> choosePhotoFromGallery()
-                        1 -> Toast.makeText(
-                            this@AddHappyPlaceActivity,
-                            "Camera selection coming soon...",
-                            Toast.LENGTH_SHORT
-                        ).show()
+                        1 -> Toast.makeText(this@AddHappyPlaceActivity,"Camera selection coming soon...", Toast.LENGTH_SHORT).show()
                     }
                 }
                 pictureDialog.show()
@@ -76,6 +96,7 @@ class AddHappyPlaceActivity : AppCompatActivity(), View.OnClickListener {
 
         }
     }
+
 
     private fun updateDateInView() {
         val myFormat = "dd.MM.yyyy"
@@ -97,11 +118,7 @@ class AddHappyPlaceActivity : AppCompatActivity(), View.OnClickListener {
 
                     if (report!!.areAllPermissionsGranted()) {
 
-                        Toast.makeText(
-                            this@AddHappyPlaceActivity,
-                            "Storage READ/WRITE permission are granted. Now you can select an image from GALLERY or lets says phone storage.",
-                            Toast.LENGTH_SHORT
-                        ).show()
+                        Toast.makeText(this@AddHappyPlaceActivity,"Storage READ/WRITE permission are granted. Now you can select an image from GALLERY or lets says phone storage.", Toast.LENGTH_SHORT).show()
                     }
                 }
 
@@ -109,17 +126,20 @@ class AddHappyPlaceActivity : AppCompatActivity(), View.OnClickListener {
                     permissions: MutableList<PermissionRequest>?,
                     token: PermissionToken?
                 ) {
-                    showRationalDialogPermissions()
+                    showRationalDialogForPermissions()
                 }
             }).onSameThread()
             .check()
-
+        // END
     }
+    // END
 
-    private fun showRationalDialogPermissions() {
-        AlertDialog.Builder(this)
-            .setMessage("Its looks like you haven't set permission required for this feature. It can be enabled Applications Settings")
-            .setPositiveButton("Go TO SETTINGS") { _, _ ->
+
+    private fun showRationalDialogForPermissions() {
+        androidx.appcompat.app.AlertDialog.Builder(this)
+            .setMessage("It Looks like you have turned off permissions required for this feature. It can be enabled under Application Settings")
+            .setPositiveButton("GO TO SETTINGS"
+            ) { _, _ ->
                 try {
                     val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
                     val uri = Uri.fromParts("package", packageName, null)
