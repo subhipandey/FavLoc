c78aeeecd9f3151c53925999a90d0a4a1e0c99ca
Subhi Pandey <subhi2012cpi@gmail.com>
2020-07-30 18:34:25 +0530

    Created HappyPlacesAdapte and ui

--- app/src/main/java/com/subhipandey/android/favloc/activities/MainActivity.kt
+++ app/src/main/java/com/subhipandey/android/favloc/activities/MainActivity.kt
@@ -3,34 +3,60 @@ package com.subhipandey.android.favloc.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.subhipandey.android.favloc.R
+import com.subhipandey.android.favloc.adapters.HappyPlacesAdapter
 import com.subhipandey.android.favloc.database.DatabaseHandler
 import com.subhipandey.android.favloc.models.HappyPlaceModel
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
+
+
         setContentView(R.layout.activity_main)
 
+
         fabAddHappyPlace.setOnClickListener {
-            val intent = Intent(this, AddHappyPlaceActivity::class.java)
+            val intent = Intent(this@MainActivity, AddHappyPlaceActivity::class.java)
             startActivity(intent)
         }
+
         getHappyPlacesListFromLocalDB()
     }
 
-    private fun getHappyPlacesListFromLocalDB(){
-        val dbHandler = DatabaseHandler(this)
-        val getHappyPlaceList : ArrayList<HappyPlaceModel> = dbHandler.getHappyPlacesList()
 
-        if(getHappyPlaceList.size > 0){
-            for(i in getHappyPlaceList){
-                Log.e("Title", i.title)
-                Log.e("Description", i.description)
-            }
+    private fun getHappyPlacesListFromLocalDB() {
+
+        val dbHandler = DatabaseHandler(this)
+
+        val getHappyPlacesList = dbHandler.getHappyPlacesList()
+
+
+        if (getHappyPlacesList.size > 0) {
+            rv_happy_places_list.visibility = View.VISIBLE
+            tv_no_records_available.visibility = View.GONE
+            setupHappyPlacesRecyclerView(getHappyPlacesList)
+        } else {
+            rv_happy_places_list.visibility = View.GONE
+            tv_no_records_available.visibility = View.VISIBLE
         }
+
+    }
+
+
+    private fun setupHappyPlacesRecyclerView(happyPlacesList: ArrayList<HappyPlaceModel>) {
+
+        rv_happy_places_list.layoutManager = LinearLayoutManager(this)
+        rv_happy_places_list.setHasFixedSize(true)
+
+        val placesAdapter = HappyPlacesAdapter(this, happyPlacesList)
+        rv_happy_places_list.adapter = placesAdapter
     }
 
 }
